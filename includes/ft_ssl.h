430d81730c9ef52c44c1e865e8579729fc37acba
owalid <othmane.el.ayadi@gmail.com>
2022-12-21 14:50:54 +0100

    feat: md5 ok

--- includes/ft_ssl.h
+++ includes/ft_ssl.h
@@ -9,8 +9,8 @@
 
 
 void     md5_process(char *input);
-void    md5_process_firsts_blocks(unsigned int *w, int *vars);
-void    md5_process_last_block(char *input, int *vars);
-int     left_rotate(int n, unsigned int d);
+void    md5_process_firsts_blocks(unsigned int *w, unsigned int *vars);
+void    md5_process_last_block(char *input, unsigned int *vars);
+unsigned int     left_rotate(unsigned int n, unsigned int d);
 void print_bits(unsigned char *str, size_t len);
 #endif
