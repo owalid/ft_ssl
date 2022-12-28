583c0bb4c086395f1844aa2d5417ec65d642c620
owalid <othmane.el.ayadi@gmail.com>
2022-12-28 13:30:19 +0100

    feat: ok

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@
 
 void ft_search_modes(char **argv, int argc, t_ft_ssl_mode *ssl_mode) {
     for (int i = 0; i < argc; i++) {
+        if (ft_strcmp(argv[i], "--") == 0)
+            break;
         if (ft_strcmp(argv[i], "-q") == 0) {
             ssl_mode->quiet_mode = 1;
         } else if (ft_strcmp(argv[i], "-r") == 0) {
@@ -36,11 +38,11 @@ int main(int argc, char **argv) {
                 int j = 2;
 
                 for (; j < argc; j++) {
-                    // if (ft_strcmp(argv[j], "-p") == 0) {
-                    //     j++;
-                    //     break;
-                    // }
-                    if (ft_strstr(argv[j], "-") == NULL && ft_strcmp(argv[j-1], "-s") != 0) // check if is a file
+                    if (ft_strcmp(argv[j], "--") == 0) {
+                        j++;
+                        break;
+                    }
+                    if ((ft_strstr(argv[j], "-") == NULL && ft_strcmp(argv[j-1], "-s") != 0)) // check if is a file
                         break;
                     if (ft_strcmp(argv[j], "-s") == 0 && (j + 1) < argc) { // process as string
                         g_ftssl_op[i].ft_ssl_process(argv[j + 1], ssl_mode, 0, g_ftssl_op[i].name);
@@ -52,8 +54,8 @@ int main(int argc, char **argv) {
                     g_ftssl_op[i].ft_ssl_process(argv[j], ssl_mode, 1, g_ftssl_op[i].name);
                     flag_process = 1;
                 }
-
                 if (flag_process == 0 || ssl_mode->std_mode == 1) {
+                    printf("lol");
                     g_ftssl_op[i].ft_ssl_process(NULL, ssl_mode, 2, g_ftssl_op[i].name);
                 }
             }
